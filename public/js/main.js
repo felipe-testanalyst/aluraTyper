abd2ae9533fdb2dd4da0e44f668d6d6b399f1349
felipe-testanalyst <FPEREIRA@tfal.com.au>
2020-04-24 08:33:22 +1000

    Jogo inicial

---
+++ public/js/main.js
@@ -0,0 +1,81 @@
+var campo = $(".campo");
+var tempoInicial = $("#tempo-digitacao").text();
+
+$(function(){                                      //funcao de atalho que inicia as funcoes assim que a pagina e carregada
+    atualizaTamanhofrase();
+    inicializaContadores();
+    inicializaCronometro();
+    inicializaMarcadoresBorda();
+    $("#botao-reiniciar").click(reiniciaJogo);
+})
+
+function atualizaTamanhofrase(){
+    var frase = $(".frase").text();                //pega frase
+    var numPalavras = frase.split(" ").length;    //divide as palavras e conta o tamanho
+
+    var tamanhofrase = $("#tamanho-frase");       //pega o span para o numero de palavras
+    tamanhofrase.text(numPalavras);              // adiciona o numPalavras dentro do span
+}
+
+
+function inicializaContadores(){
+    campo.on("input", function(){                             //jQuery escutador de eventos
+        var conteudo = campo.val();                          //pega o valor do textarea
+        var qtdPalavras = conteudo.split(/\S+/).length -1 ; //divide as palavras com expresao regular que verifica espacos
+        $("#numero-palavras").text(qtdPalavras);           //coloca a qtdPalavas no span
+    
+        var conteudoCaracteres = conteudo.length;
+        $("#numero-caracteres").text(conteudoCaracteres);
+    
+    })
+}
+
+function inicializaCronometro(){
+    var tempoRestante = $("#tempo-digitacao").text(); 
+    campo.one("focus", function(){                     //diferente do campo.on o campo.one escuta o evento somente uma vez
+        $("#botao-reiniciar").attr("disabled", true);  //disabilita o botao reiniciar
+        var cronometroID = setInterval(function(){       // setInterval inicia o intervalo de tempo com 1000
+            tempoRestante--;                             //diminui tempo restante a cada 1000 mls
+            $("#tempo-digitacao").text(tempoRestante);  
+            if(tempoRestante < 1){                        
+                clearInterval(cronometroID);             //para o setInterval
+                finalizaJogo();
+                inserePlacar();
+            }
+        }, 1000);
+    });
+}
+
+function finalizaJogo(){
+    campo.attr("disabled", true);            //adiciona atributo disabled no textarea para parar digitacao
+    $("#botao-reiniciar").attr("disabled", false); //abilita botao reiniciar
+    campo.addClass("campo-desativado");
+}
+
+function inicializaMarcadoresBorda(){
+    var frase = $(".frase").text();
+    campo.on("input", function(){
+        var digitado = campo.val();
+        var comparavel = frase.substr(0, digitado.length); // compara a substring da frase com o que foi digitado
+        if(digitado == comparavel){
+            campo.addClass("borda-verde");
+            campo.removeClass("borda-vermelha");
+        }
+        else{
+            campo.addClass("borda-vermelha");
+            campo.removeClass("borda-verde");
+        }
+    });
+}
+
+function reiniciaJogo(){
+    campo.attr("disabled", false);
+    campo.val("");
+    $("#numero-palavras").text("0");
+    $("#numero-caracteres").text("0");
+    $("#tempo-digitacao").text(tempoInicial);
+    inicializaCronometro();
+    campo.removeClass("campo-desativado");
+    campo.removeClass("borda-verde");
+    campo.removeClass("borda-vermelha");
+}
---
+++ public/js/placar.js
@@ -0,0 +1,36 @@
+function inserePlacar(){
+    var placar = $(".placar");
+    var corpoTabela = placar.find("tbody");      //busca pelo tbody dentro de placar e coloca na variavel corpoTabela
+    var usuario = "Felipe";
+    var numPalavras = $("#numero-palavras").text();
+
+    var linha = novaLinha(usuario, numPalavras); 
+    linha.find(".botao-remover").click(removeLinha);   //busca pelo elemento que contem classe dentro da linha e chama a funcao remove linha no clique
+    corpoTabela.prepend(linha);                       //adiciona linha retornada da funcao novaLinha no inicio do corpoTabela
+}
+
+
+function novaLinha(usuario, numPalavras){
+    var linha = $("<tr>");                              //cria uma tr usando jQuery
+    var colunaUsuario = $("<td>").text(usuario);        //cria uma td recebendo o usuario da funcao insere placar e adiciona na colunaUsuario
+    var colunaPalavras = $("<td>").text(numPalavras);   
+    var colunaRemover = $("<td>");
+
+    var link = $("<a>").attr("href", "#").addClass("botao-remover");
+    var icone = $("<i>").addClass("small").addClass("material-icons").text("delete");
+
+    link.append(icone);           //icone dentro do <a>
+    colunaRemover.append(link);   //<a> dentro do <td>
+
+    //os tres <td> dentro do <tr>
+    linha.append(colunaUsuario);
+    linha.append(colunaPalavras);
+    linha.append(colunaRemover);
+
+    return linha;                                      //retorna a linha para a funcao que chamou
+}
+
+function removeLinha(event){   
+    event.preventDefault();                    
+    $(this).parent().parent().remove();       //remove o avo do elemento que foi clicado (usado na funcao insere placar)
+}
