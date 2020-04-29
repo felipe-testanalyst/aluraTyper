b93072a772184790992349a58233360786d5d8bb
Felipe Pereira <setimo.felipe@gmail.com>
2020-04-30 07:57:17 +1000

    error handling do server e spinner

--- public/js/frase.js
+++ public/js/frase.js
@@ -1,7 +1,18 @@
 $("#botao-frase").click(fraseAlearoria); //pega botao e agrega evento de click
 
 function fraseAlearoria(){
-    $.get("http://localhost:3000/frases", trocaFraseAleatoria);  //requisicao ajax e chama funcao para trocarfrase
+
+    $("#spinner").toggle(); //mostra spinner
+
+    $.get("http://localhost:3000/frases", trocaFraseAleatoria)  //requisicao ajax e chama funcao para trocarfrase
+    .fail(function(){                                            //caso o .get fail inicia funcao
+        $("#erro").toggle();                                    //mostra msg erro do html
+        setTimeout(function(){                                  // set timer 
+            $("#erro").toggle();                                //toggle msg de erro depois de 2 segundos
+        }, 2000); 
+    }).always(function(){                                       //always sempre executa funcao
+        $("#spinner").toggle();                                 // toggle spinner
+    })
 }
 
 function trocaFraseAleatoria(dados) {   //traz dados da requisicao ajax
